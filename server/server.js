c5043c87faf79771a9a77ec9ff327a9a158e28e8
Henry Owen <hnryown@gmail.com>
2023-01-25 11:59:36 -0500

    add build artifacts and bundlejs

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static(publicPath));
 
